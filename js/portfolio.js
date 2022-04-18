06208e65145bd6f953bdc254737cc7d3ec4b8d73
Ethan Connelly <connellyethan@gmail.com>
2022-04-18 14:44:53 -0400

    added slide in and fade in features

--- js/portfolio.js
+++ js/portfolio.js
@@ -50,6 +50,34 @@ const navSlide = () => {
 
 }
 
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.from-bottom');
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target)
+        }
+
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
+
 const typedTextSpan = document.querySelector(".typed-text");
 
 const textArray = ["web developer.", "UI designer.", "avid fantasy reader.", "agile team worker.", "gamer.", "perfectionist.", "gym lover."];
